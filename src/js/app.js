1f11277a9478e08931f2938acd1a5534e6dae62c
Victor Punko <workvictor@yandex.ru>
2017-05-08 09:31:28 +0300

    ie9 warning added

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,18 @@
 import '../css/main.scss';
 import Noise from './noise';
 import Display from './display';
-
+if (window.requestAnimationFrame === undefined) {
+    let ie9Warning = document.getElementById('ie9Warning');
+    ie9Warning.style.display = 'block';
+}else
 main();
 
 function main() {
+
     let forDisplay = document.getElementById('forDisplay');
     let forLoading = document.getElementById('forLoading');
 
     let display = new Display(640);
     display.setParent(forDisplay);
-    var noise = new Noise(display,forLoading);
+    var noise = new Noise(display, forLoading);
 }
